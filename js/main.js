6929f30619aee2796e603f4650c7d542bcc79421
mexeri <zmecseri@gmail.com>
2022-06-03 01:15:08 +0200

    2.commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,26 @@ function calcAmount()
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
-    let amount = parseInt(amountInput.value)*price;
+    let amountNumber = parseInt(amountInput.value);
     
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    
+    if(amountNumber>10)
+    {
+        alert("Max 10 db!");
+        return;
+    }
+    else if(amountNumber<1)
+    {
+        alert("Min 1 db kell!");
+        return;
+    }
+    
+    
+    
+    let amount = amountNumber*price;
+    
+
     showAmount.innerHTML = amount;
 }
